a1f7e79706ce79b025fe3bc6519f3e0df7b9a6fd
victor <victorhugoocordeirorocha@gmail.com>
2023-08-23 21:43:49 -0400

    parte 1

---
+++ treinos/treino4_calculator.py
@@ -0,0 +1,10 @@
+while True:
+    
+    
+
+
+    sair = input('Você deseja [S]air? <<< ').lower().startswith('s') # islower é para deixar tudo minusculo, startswhith é para verificar a primeira letra
+
+    if sair is True:
+        break
+    
