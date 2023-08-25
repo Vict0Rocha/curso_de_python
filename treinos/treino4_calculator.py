4450a905697032f9934ef0ea46450013109667dc
victor <victorhugoocordeirorocha@gmail.com>
2023-08-24 20:40:58 -0400

    calculadora pronta Porém está com erro, não executas as operações de forma correta

--- treinos/treino4_calculator.py
+++ treinos/treino4_calculator.py
@@ -1,13 +1,16 @@
 while True:
     n1 = input('Digite o PRIMEIRO valor <<< ')
     n2 = input('Digite o SEGUNDO valor <<< ')
-    operado = input('Digite o OPERADOR (+-*/) <<< ')
+    operador = input('Digite o OPERADOR (+-*/) <<< ')
     valores_validos = None #None quer dizer, vazio
+    n1_float = 0
+    n2_float = 0
 
     try:
         n1_float = float(n1)
         n2_float = float(n1)
         valores_validos = True
+
     except:
         valores_validos = None
 
@@ -16,12 +19,24 @@ while True:
         continue #Para voltar ao inicio do código      
     operadores_permitidos = '+-*/'
 
-    if operado not in operadores_permitidos:
+    if operador not in operadores_permitidos:
         print('Operador inválido.')
         continue     
-    if len(operado) > 1:
+
+    if len(operador) > 1:
         print('Digite apenas um operador.')
         continue
+     
+    if operador == '+':
+        print(n1_float+n2_float)
+    elif operador == '-':
+        print(n1_float-n2_float)
+    elif operador == '*':
+        print(n1_float*n2_float)
+    elif operador == '/':
+        print(n1_float/n2_float)
+    else:
+        print('Nunca deveria chegar aqui!')
 
     sair = input('Você deseja [S]air? <<< ').lower().startswith('s') # islower é para deixar tudo minusculo, startswhith é para verificar a primeira letra 
     if sair is True:
