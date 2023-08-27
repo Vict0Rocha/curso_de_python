570c8cc24b523bb452a48938fae161c6285714f7
victor <victorhugoocordeirorocha@gmail.com>
2023-08-27 16:18:35 -0400

    Jogo de adivinhe a palavra segreta Parte 1 concluida

---
+++ treinos/jogo_palavra_secreta.py
@@ -0,0 +1,22 @@
+palavra_secreta = 'python'
+
+print('-'*27)
+print('ADIVINHE A PALAVRA SEGRATA')
+print('-'*27)
+ 
+palpite = ' '
+tentativas = 0
+
+while palpite != palavra_secreta:
+    palpite = input('Digite seu palpite <<< ')
+
+    if len(palpite) > 1:
+        print('[ERROS] - palpite não salvo')
+        print('Digite apenas uma letra!')
+        continue
+
+    # for i in palavra_secreta:
+    #     if palpite == i:
+    #         print('ok')
+    print('Errou')
+
