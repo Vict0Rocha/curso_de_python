ab8f870450cb01fa7cf36bce76d554440a2122ef
Victor Cordeiro <107584325+Vict0Rocha@users.noreply.github.com>
2023-08-28 16:50:40 -0400

    Atualização do jogo_palavra_secreta.py

--- treinos/jogo_palavra_secreta.py
+++ treinos/jogo_palavra_secreta.py
@@ -1,22 +1,41 @@
+import os
+
 palavra_secreta = 'python'
-
-print('-'*27)
-print('ADIVINHE A PALAVRA SEGRATA')
-print('-'*27)
- 
-palpite = ' '
+palpite = ''
 tentativas = 0
+jogo =True
+letra_certa = ''
+palavra_formada = ''
 
-while palpite != palavra_secreta:
-    palpite = input('Digite seu palpite <<< ')
-
+while jogo == True:
+    print('Adivinhe a palavra secreta')
+    
+    # for letra_palpite in palavra_secreta:
+    #     print(end = (' _ '))
+    palpite = input('Digite uma letra: ')
+    
+    tentativas += 1
+    
     if len(palpite) > 1:
-        print('[ERROS] - palpite não salvo')
-        print('Digite apenas uma letra!')
+        print('Digite apenas uma letra')
         continue
-
-    # for i in palavra_secreta:
-    #     if palpite == i:
-    #         print('ok')
-    print('Errou')
-
+    
+    if palpite in palavra_secreta:
+       letra_certa += palpite
+       
+    palavra_formada = ''
+    for letra_palpite in palavra_secreta:
+        if letra_palpite in letra_certa:
+           palavra_formada += letra_palpite
+        else:
+             palavra_formada += '*'
+        
+    print(palavra_formada)
+    
+    if palavra_formada == palavra_secreta:
+        os.system('clear')
+        print('Parabéns, VOCÊ GANHOU!')
+        print('A palavra segreta era', palavra_secreta)
+        print('Total de tentativas:', tentativas)
+        tentativas = 0
+        letra_certa = ''
