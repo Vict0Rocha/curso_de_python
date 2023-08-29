7c394b171b78d14e196afd86880cc7f5ea8bb0bc
victor <victorhugoocordeirorocha@gmail.com>
2023-08-29 13:03:08 -0400

    Metodos em listas

--- aulas/aula18_list.py
+++ aulas/aula18_list.py
@@ -29,3 +29,5 @@ lista_b = [4, 5, 6]
 lista_c = lista_a + lista_b
 lista_a.extend(lista_b)
 print(lista_a)
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy() #Para copiar uma lista
