0a3375c642e0ca216702d5e6aa54dd25b096cf6d
victor <victorhugoocordeirorocha@gmail.com>
2023-08-29 13:01:27 -0400

    Metodos em listas

--- aulas/aula18_list.py
+++ aulas/aula18_list.py
@@ -21,3 +21,11 @@ print(lista)
 # lista.clear()
 lista.insert(100, 5)
 print(lista[4])
+
+#Concatenando str
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
