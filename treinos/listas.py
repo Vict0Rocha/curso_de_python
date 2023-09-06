710a24bf1d8bbb7342fd3843035dcf9a157459cc
victor <victorhugoocordeirorocha@gmail.com>
2023-09-05 20:41:46 -0400

    Parte 1 da lista pronta

---
+++ treinos/listas.py
@@ -0,0 +1,21 @@
+lista = []
+condicao = True
+
+while condicao == True:
+    print('-'*15)
+    print('FAÇA UMA LISTA')
+    print('-'*15)
+    print('\nDigite oque deseja realizar')
+    print('[A]adicionar [D]eletar [L]listar [S]sair')
+    escolha = input(' ')
+    if escolha == 'a':
+        ...
+    elif escolha == 'd':
+            ...
+    elif escolha == 'l':
+         ...
+    elif escolha == 's':
+         break
+    else:
+        print('Digite somente: A, D, ou L')
+        
