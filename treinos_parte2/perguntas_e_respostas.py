5c7bee7f7cc409ae93072963377f24ea8d272a33
victor <victorhugoocordeirorocha@gmail.com>
2023-09-11 21:43:55 -0400

    Jogo de perguntas e respostas

--- treinos_parte2/perguntas_e_respostas.py
+++ treinos_parte2/perguntas_e_respostas.py
@@ -3,7 +3,7 @@
 perguntas_e_repostas = [
     {
         'Pergunta': 'Quanto é 2+2?',
-        'Opções': ['1', '2', '3', '4'],
+        'Opções': ['1', '4', '3', '8'],
         'Resposta': '4',    
     },
     {
@@ -18,11 +18,34 @@ perguntas_e_repostas = [
     },
 ]
 
+qtd_acertos = 0
 for pergunta in perguntas_e_repostas: #Percorrendo toda a lista de pergunta e respostas
-    print('Pergunta: ', pergunta['Pergunta']) # Escrevendo o valor de cada pergunta
-    # print('Opções: ', pergunta['Opções'])
+    print('Pergunta:', pergunta['Pergunta']) # Escrevendo o valor de cada pergunta
+    print()
     
     opcoes = pergunta['Opções']
-    for i, opcoes in enumerate(opcoes):
-        print(f'{i}) {opcoes}')
-    print('')
+    for i, opcao in enumerate(opcoes):
+        print(f'{i}) {opcao}')
+    print()
+
+    escolha = input('Escolha uma opção: ')
+    escolha_int = None
+    acerto = False
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+    
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acerto = True
+    
+    print()
+    if acerto:
+        qtd_acertos += 1
+        print('Acertou ✅ ')
+    else:
+        print('Errou ❌ ')
+
+print(f'Você acertou {qtd_acertos} de {len(pergunta)} perguntas!')
