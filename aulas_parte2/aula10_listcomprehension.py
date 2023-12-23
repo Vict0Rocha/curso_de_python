07a6ffcd26671c238b9d776adc30df1d1678d6c2
victor <victorhugoocordeirorocha@gmail.com>
2023-12-23 15:42:01 -0400

    list comprehension

---
+++ aulas_parte2/aula10_listcomprehension.py
@@ -0,0 +1,15 @@
+# List comprehension é uma foma rápida
+# para criar lista a partir de iteráveis.
+
+lista = []
+
+# for numero in range(10):
+#     lista.append(numero)
+
+# Lista   valor         valor no range de 10
+lista = [numero * 2 for numero in range(10)]
+
+# Estou adicionando na minha lista os valores de 0 a 9
+# multiplicado por 2.
+
+print(lista) 
