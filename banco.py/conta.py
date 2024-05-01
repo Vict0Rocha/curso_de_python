58a5f2a36141306247d7c404da5398906dd3f9bd
victor <victorhugoocordeirorocha@gmail.com>
2024-05-01 13:45:56 -0400

    Classes contas

--- banco.py/conta.py
+++ banco.py/conta.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Conta(ABC): #balance -> Saldo 
-    def __init__(self, agencia, number, balance):
+    def __init__(self, agencia, number, balance = 0):
         self.agencia = agencia
         self.number = number
         self.balance = balance
@@ -15,8 +15,8 @@ class Conta(ABC): #balance -> Saldo
         self.detalhes()
         return self.balance
 
-    def detalhes(self, msg: str = '') -> None:
-        print(f'O seu saldo é R${self.balance:.2f} {msg}')
+    def detalhes(self) -> None:
+        print(f'O seu saldo é R${self.balance:.2f}')
 
 class ContaCorrente(Conta):
     def __init__(self, agencia, number, balance, limit: float):
@@ -27,14 +27,24 @@ class ContaCorrente(Conta):
         valor_pos_saque = self.balance - value_saque
         maximum_limit = -self.limit
         if valor_pos_saque >= maximum_limit:
-            ...
+            self.balance -= value_saque
+            print(f'Saque bem sucedido no valor de R${value_saque}')
+            self.detalhes()
+            return self.balance
+        print('SAQUE NEGADO - Valor indisponivel para saque.')
+        self.detalhes()
+        
 
 class ContaPoupanca(Conta):
     def __init__(self, agencia, number, balance):
         super().__init__(agencia, number, balance)
     
-    def sacar(self, value: float) -> float:
-        ...
-
-cc1 = ContaCorrente(111, 222, 0, 10)
-cc1.depositar(10) 
+    def sacar(self, value_saque: float) -> float:
+        if self.balance >= value_saque:
+            self.balance -= value_saque
+            print(f'SAQUE BEM SUCEDIDO no valor de R${value_saque}')
+            self.detalhes()
+            return self.balance
+            
+        print('SAQUE NEGADO - Valor indisponivel para saque.')
+        self.detalhes()
