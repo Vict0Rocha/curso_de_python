d7d8e152fc28f6a0b68c5927919bc2096c853bc5
victor <victorhugoocordeirorocha@gmail.com>
2024-05-04 13:58:05 -0400

    Data Class

---
+++ aulas_parte3/aula9_data_classes.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass(init=True)
+class Pessoa:
+    nome: str
+    sobrenome: str
+    nome_completo: str | None = None
+
+    # def __init__(self, nome, sobrenome):
+    #     self.nome = nome
+    #     self.sobrenome = sobrenome
+    #     self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+    # def __post_init__(self):
+    #     print('POST INIT')
+
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
+
+    # @nome_completo.setter
+    # def nome_completo(self, valor):
+    #     nome, *sobrenome = valor.split()
+    #     self.nome = nome
+    #     self.sobrenome = ' '.join(sobrenome)
+
+if __name__ == '__main__':
+    lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
+    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
+    print(ordenadas)
